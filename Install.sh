bfb9309a6878d97982dc96bd09228199f5abaf95
LeonardoBaldazzi <public.thesquirlyz97@gmail.com>
2019-03-22 20:27:25 +0100

    Resolved qemu install error

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install build-essential
 sudo apt-get install xorriso
 sudo apt-get install grub
-sudo apt-get install qemu*
 sudo apt-get install git
+sudo apt-get install qemu-kvm qemu virt-manager virt-viewer libvirt-bin
